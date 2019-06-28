b920bc5b5b9a7f0266bedf96b1aeef34bbdb37bc
IamRohitMittal <rohitmittal204@gmail.com>
2019-06-28 10:17:55 +0530

    Add files via upload

---
+++ Elections.sol
@@ -0,0 +1,10 @@
+//solium-disable linebreak-style
+pragma solidity ^0.5.0;
+
+contract Election{
+         
+    string public candidate;
+    constructor() public{
+        candidate = "Lalu";  
+    }
+}  
